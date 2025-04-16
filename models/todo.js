f78229bf435276a66636e002233b1d70be48e2ca
prinsi-t <tprincy56@gmail.com>
2025-04-16 08:32:07 +0530

    new commit

--- models/todo.js
+++ models/todo.js
@@ -10,11 +10,29 @@ const todoSchema = new mongoose.Schema({
     type: Boolean,
     default: false,
   },
+  notes: {
+    type: String,
+    default: '',
+  },
+  list: {
+    type: String,
+    default: 'Personal',
+    enum: ['Personal', 'Work', 'Grocery List'],
+  },
+  subtasks: [{
+    title: String,
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  }],
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
   },
+}, {
+  timestamps: true,
 });
 
 const Todo = mongoose.models.Todo || mongoose.model('Todo', todoSchema);
